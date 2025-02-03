16999e25effaedc4769c574a982c04e8e19f3403
Cole Blender <coleblender@gmail.com>
2025-02-02 17:52:37 -0800

    done

---
+++ src/components/NewNoteButton.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { User } from "@supabase/supabase-js";
+import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
+import { useToast } from "@/hooks/use-toast";
+import { debounceTimeout } from "@/lib/constants";
+import { createNoteAction } from "@/actions/notes";
+
+type Props = {
+  user: User | null;
+};
+
+function NewNoteButton({ user }: Props) {
+  const router = useRouter();
+
+  const { toast } = useToast();
+
+  const [loading, setLoading] = useState(false);
+
+  const handleClickNewNoteButton = async () => {
+    if (!user) {
+      router.push("/login");
+    } else {
+      setLoading(true);
+      const savingToast = toast({
+        title: "Saving Current Note",
+        description: "Saving your current note before creating a new one",
+        variant: "default",
+      });
+
+      await new Promise((resolve) =>
+        setTimeout(resolve, debounceTimeout + 500),
+      );
+
+      const uuid = uuidv4();
+      await createNoteAction(uuid);
+      router.push(`/?noteId=${uuid}`);
+
+      savingToast.dismiss();
+      toast({
+        title: "New Note Created",
+        description: "You have created a new note",
+        variant: "success",
+      });
+
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Button
+      onClick={handleClickNewNoteButton}
+      variant="secondary"
+      className="w-24"
+      disabled={loading}
+    >
+      {loading ? <Loader2 className="animate-spin" /> : "New Note"}
+    </Button>
+  );
+}
+
+export default NewNoteButton;
