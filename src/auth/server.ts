b27daed256d44819f77058da36b54836932cedc0
emailhpwiko <emailhpwiko@gmail.com>
2025-04-21 04:16:44 +0000

    tambah use server sama dev.nix

--- src/auth/server.ts
+++ src/auth/server.ts
@@ -1,3 +1,5 @@
+'use server'
+
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
@@ -17,7 +19,9 @@ export async function createClient() {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options),
             );
-          } catch {}
+          } catch(error) {
+            console.log(error)
+          }
         },
       },
     },
